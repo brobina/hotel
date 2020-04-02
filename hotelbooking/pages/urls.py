34012670a2ad750cb36c05fb0267ccc1d073161d
brobina <bajracharya.robina@gmail.com>
2020-04-02 16:56:08 +0545

    initial commit

---
+++ hotelbooking/pages/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from django.http import HttpResponse
+from . import views
+urlpatterns=[
+	path('',views.index, name='index'),
+	path('about',views.about, name='about'),
+]
